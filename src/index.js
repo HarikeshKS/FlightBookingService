4d7f81e05aaa3ae643891e222df26d712fb86031
Harikesh Kumar Sharma <sharmaharikeshkumar3@gmail.com>
2024-09-09 17:56:50 +0530

    Added more packages and created model

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 const express = require("express");
-const { PORT } = require("./config/server-config");
-function startAndSetup() {
-    const app = express();
+const bodyParser = require("body-parser");
+const { PORT, DB_SYNC } = require("./config/server-config");
+const bodyParser = require("body-parser");
+const app = express();
+
+const apiRoutes = require("./routes/index");
+
+const db = require("./models/index");
+
+function setupAndStartServer() {
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.use("/api", apiRoutes);
 
     app.listen(PORT, () => {
         console.log(`Server Started at PORT: ${PORT}`);
+        
+        if (DB_SYNC) {
+            db.sequelize.sync({ alert: true });
+        }
     });
+    
 }
 
-startAndSetup();
+setupAndStartServer();
