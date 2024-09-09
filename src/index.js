45aceaf08691e9b113fc233e6e6432afc30c265f
Harikesh Kumar Sharma <sharmaharikeshkumar3@gmail.com>
2024-09-09 23:24:21 +0530

    Added update table migration for noOfSeats and totalCost

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const { PORT, DB_SYNC } = require("./config/server-config");
 const bodyParser = require("body-parser");
 const app = express();
